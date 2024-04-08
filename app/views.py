eb3eb6000124f0c31f599c85053df17e7116c908
Ali Hamza <hafizalihamza047@gmail.com>
2024-04-09 00:33:34 +0500

    convert into django templetes

---
+++ app/views.py
@@ -0,0 +1,113 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request, 'index.html')
+
+def SmartContractAudit(request):
+    return render(request, 'SmartContractAudit.html')
+
+def BlockchainProtocolSecurity(request):
+    return render(request, 'BlockchainProtocolSecurity.html')
+
+def PenetrationTesting(request):
+    return render(request, 'PenetrationTesting.html')
+
+def dAppAudit(request):
+    return render(request, 'dAppAudit.html')
+
+def CryptoWalletAudit(request):
+    return render(request, 'CryptoWalletAudit.html')
+    
+def CrossChainBridgeAudit(request):
+    return render(request, 'Cross-ChainBridgeAudit.html')
+
+def BugBounty(request):
+    return render(request, 'BugBounty.html')
+
+def ProofOfReservesAudit(request):
+    return render(request, 'ProofOfReservesAudit.html')
+
+def CCSSAudit(request):
+    return render(request, 'CCSSAudit.html')
+
+def TokenomicsAuditDesign(request):
+    return render(request, 'TokenomicsAuditDesign.html')
+
+def Solutions(request):
+    return render(request, 'Solutions.html')
+
+def SolutionsBSC(request):
+    return render(request, 'SolutionsBSC.html')
+
+def SolutionsPolygon(request):
+    return render(request, 'SolutionsPolygon.html')
+
+def SolutionsNear(request):
+    return render(request, 'SolutionsNear.html')
+
+def SolutionsAvalanche(request):
+    return render(request, 'SolutionsAvalanche.html')
+
+def SolutionsFantom(request):
+    return render(request, 'SolutionsFantom.html')
+
+def SolutionsAptosSmart(request):
+    return render(request, 'SolutionsAptosSmart.html')
+
+def SolutionsSui(request):
+    return render(request, 'SolutionsSui.html')
+
+def SolutionsOptimismContract(request):
+    return render(request, 'SolutionsOptimismContract.html')
+
+def SolutionsZkSync(request):
+    return render(request, 'SolutionsZkSync.html')
+
+def SolutionsArbitrum(request):
+    return render(request, 'SolutionsArbitrumAudit.html')
+
+def SolutionsSolana(request):
+    return render(request, 'SolutionsSolana.html')
+
+def SolutionsPolkadot(request):
+    return render(request, 'SolutionsPolkadot.html')
+
+def SolutionsCosmos(request):
+    return render(request, 'SolutionsCosmos.html')
+
+def SolutionsRadix(request):
+    return render(request, 'SolutionsRadix.html')
+
+def SolutionsLinea(request):
+    return render(request, 'SolutionsLinea.html')
+
+def SolutionsMetaMask(request):
+    return render(request, 'SolutionsMetaMask.html')
+
+def SolutionsOther(request):
+    return render(request, 'SolutionsOther.html')
+
+def SolutionsSolidity(request):
+    return render(request, 'SolutionsSolidity.html')
+
+def SolutionsRust(request):
+    return render(request, 'SolutionsRust.html')
+
+def SolutionsMove(request):
+    return render(request, 'SolutionsMove.html')
+
+def CompanyOurMission(request):
+    return render(request, 'CompanyOurMission.html')
+
+def CompanyAbout(request):
+    return render(request, 'CompanyAbout.html')
+
+def Blogs(request):
+    return render(request, 'Blogs.html')
+
+def CompanyAboutProfit(request):
+    return render(request, 'CompanyAboutProfit.html')
+
+def Career(request):
+    return render(request, 'Career.html')
